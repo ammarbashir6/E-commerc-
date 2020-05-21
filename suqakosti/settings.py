d0961cd21bedf9b982dc37d4b5183939f02f7efc
ammar <a.bashirballa@gmail.com>
2020-05-21 14:31:38 -0700

    make some change in settings

--- suqakosti/settings.py
+++ suqakosti/settings.py
@@ -21,15 +21,15 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "5eyq*t5z(nnx@8z8a=7#sj=$oy6lhg+q@rbjj-g_i@sotv)&xd"
-
+# SECRET_KEY = "5eyq*t5z(nnx@8z8a=7#sj=$oy6lhg+q@rbjj-g_i@sotv)&xd"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["souqkosti.herokuapp.com"]
 
 
-# Application definition            
+# Application definition
 
 INSTALLED_APPS = [
     "django.contrib.admin",
