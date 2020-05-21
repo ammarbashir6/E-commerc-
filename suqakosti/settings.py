8da8c8735c089172318b74788a17727b1fec8ba0
ammar <a.bashirballa@gmail.com>
2020-05-21 04:45:05 -0700

    Update Static Root

--- suqakosti/settings.py
+++ suqakosti/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 
 CARD_SESSION_ID = "card"
 
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
